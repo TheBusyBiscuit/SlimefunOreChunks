41e31ffc8b923b84bb421352f513696a84e1b64e
TheBusyBiscuit <mrCookieSlime@gmail.com>
2020-03-28 12:33:22 +0100

    Updated to latest Slimefun build

--- src/main/java/io/github/thebusybiscuit/slimefunorechunks/OreChunk.java
+++ src/main/java/io/github/thebusybiscuit/slimefunorechunks/OreChunk.java
@@ -6,7 +6,6 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
-import me.mrCookieSlime.Slimefun.Lists.SlimefunItems;
 import me.mrCookieSlime.Slimefun.Objects.Category;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunMachine;
@@ -14,40 +13,40 @@ import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
 
 public class OreChunk extends SlimefunItem {
 
-	private int amplifier;
-	private String name;
-	
-	private SlimefunMachine machine;
-	private ItemStack output;
-	
-	public OreChunk(OreChunks plugin, Category category, String id, String name, int amplifier, String texture, ItemStack output) {
-		this(plugin, category, id, name, "&7Use an Ore Crusher to turn this into Dust", amplifier, texture, RecipeType.ORE_CRUSHER, output);
-	}
-	
-	public OreChunk(OreChunks plugin, Category category, String id, String name, String lore, int amplifier, String texture, RecipeType machine, ItemStack output) {
-		super(category, new SlimefunItemStack(id, texture, "&r" + name, lore), new RecipeType(SlimefunItems.GEO_MINER), new ItemStack[0]);
-		
-		this.amplifier = amplifier;
-		this.name = name;
-		
-		this.machine = (SlimefunMachine) machine.getMachine();
-		this.output = output;
-		
-		new OreResource(new NamespacedKey(plugin, id.toLowerCase(Locale.ROOT)), this).register();
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public int getAmplifier() {
-		return amplifier;
-	}
-	
-	@Override
-	public void postRegister() {
-		if (!machine.isDisabled()) {
-			machine.addRecipe(new ItemStack[] {getItem()}, output);
-		}
-	}
+    private final int amplifier;
+    private final String name;
+
+    private final SlimefunMachine machine;
+    private final ItemStack output;
+
+    public OreChunk(OreChunks plugin, Category category, String id, String name, int amplifier, String texture, ItemStack output) {
+        this(plugin, category, id, name, "&7Use an Ore Crusher to turn this into Dust", amplifier, texture, RecipeType.ORE_CRUSHER, output);
+    }
+
+    public OreChunk(OreChunks plugin, Category category, String id, String name, String lore, int amplifier, String texture, RecipeType machine, ItemStack output) {
+        super(category, new SlimefunItemStack(id, texture, "&r" + name, lore), RecipeType.GEO_MINER, new ItemStack[0]);
+
+        this.amplifier = amplifier;
+        this.name = name;
+
+        this.machine = (SlimefunMachine) machine.getMachine();
+        this.output = output;
+
+        new OreResource(new NamespacedKey(plugin, id.toLowerCase(Locale.ROOT)), this).register();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAmplifier() {
+        return amplifier;
+    }
+
+    @Override
+    public void postRegister() {
+        if (!machine.isDisabled()) {
+            machine.addRecipe(new ItemStack[] { getItem() }, output);
+        }
+    }
 }
